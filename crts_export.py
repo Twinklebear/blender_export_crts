98c212219f94d4d6c87202b2843e61cc98ef4d76
Will Usher <willusher.life@gmail.com>
2020-03-22 18:57:04 -0600

    Fix uvs, add normals export, report error on bad material config
    Also filter out unused objects from the export

--- crts_export.py
+++ crts_export.py
@@ -11,7 +11,7 @@ bl_info = {
     "name": "crts_export",
     "author": "Will Usher",
     "blender": (2, 80, 0),
-    "version": (1, 2, 0),
+    "version": (1, 3, 0),
     "location": "File > Import-Export",
     "description": "Export the scene to a ChameleonRT scene",
     "category": "Import-Export",
@@ -19,22 +19,44 @@ bl_info = {
 }
 
 def compute_mesh_buffer_sizes(mesh):
-    # TODO: Also normals? It looks like there's some separate calc_normals_split method
-    # but I'm not sure how that'll effect the indexing
-    num_uvs = 0
-    if mesh.uv_layers.active:
-        # TODO: This assumes each vertex has only one UV, shared with all faces
-        # sharing the same vertex position. Blender does not require this,
-        # so maybe later can think about splitting vertices not sharing
-        # the same UV coords
-        num_uvs = len(mesh.vertices)
-    return (len(mesh.vertices) * 3 * 4, len(mesh.loop_triangles) * 3 * 4, num_uvs * 2 * 4)
+    unique_verts = {}
+    uvs = {}
+    normals = {}
+    for t in mesh.loop_triangles:
+        for l in t.loops:
+            vert_idx = mesh.loops[l].vertex_index
+            uv = (mesh.uv_layers.active.data[l].uv[0], mesh.uv_layers.active.data[l].uv[1])
+            uv_idx = -1
+            if uv not in uvs:
+                uvs[uv] = l
+                uv_idx = l
+            else:
+                uv_idx = uvs[uv]
+            
+            normal = (mesh.loops[l].normal[0], mesh.loops[l].normal[1], mesh.loops[l].normal[2])
+            n_idx = -1
+            if normal not in normals:
+                normals[normal] = l
+                n_idx = l
+            else:
+                n_idx = normals[normal]
+
+            idx = (vert_idx, uv_idx, n_idx)
+            if not idx in unique_verts:
+                vid = len(unique_verts)
+                unique_verts[idx] = vid
+    n_verts = len(unique_verts)
+    return (n_verts * 3 * 4, len(mesh.loop_triangles) * 3 * 4, n_verts * 2 * 4, n_verts * 3 * 4)
 
 def write_mesh_info(meshes, header, byte_offset):
     mesh_indices = {}
     for mesh in meshes:
+        if mesh.users == 0:
+            continue
+
         mesh.calc_loop_triangles()
-        verts_size, indices_size, uvs_size = compute_mesh_buffer_sizes(mesh)
+        mesh.calc_normals_split()
+        verts_size, indices_size, uvs_size, normals_size = compute_mesh_buffer_sizes(mesh)
 
         positions_view = len(header["buffer_views"])
         header["buffer_views"].append({
@@ -61,6 +83,16 @@ def write_mesh_info(meshes, header, byte_offset):
                 "type": "VEC2_F32"
             })
             byte_offset += uvs_size
+            
+        normals_view = -1
+        if normals_size > 0:
+            normals_view = uvs_view + 1
+            header["buffer_views"].append({
+                "byte_offset": byte_offset,
+                "byte_length": normals_size,
+                "type": "VEC3_F32"
+            })
+            byte_offset += normals_size
 
         mesh_indices[mesh.name] = len(header["meshes"])
         mesh_info = {
@@ -70,12 +102,17 @@ def write_mesh_info(meshes, header, byte_offset):
         }
         if uvs_view != -1:
             mesh_info["texcoords"] = uvs_view
+        if normals_view != -1:
+            mesh_info["normals"] = normals_view
         header["meshes"].append(mesh_info)
     return byte_offset, mesh_indices
 
 def write_image_info(images, header, byte_offset):
     image_indices = {}
     for img in bpy.data.images:
+        if img.users == 0:
+            continue
+        
         # TODO: For generated textures how could we bake them to images?
         # we could evaluate it to create a new image, pack that image into
         # the blend file temporarily to generate a PNG we can embed, then
@@ -130,6 +167,9 @@ def get_seprgb_texture_info(link):
 def write_material_info(materials, header, image_indices):
     material_indices = {}
     for m in materials:
+        if m.users == 0:
+            continue
+        
         # No support for shader node graphs/etc. just take the principled BSDF node
         principled_node = None
         for n in m.node_tree.nodes:
@@ -210,23 +250,54 @@ def write_object_info(objects, header, material_indices, mesh_indices):
 
 
 def write_mesh_buffers(mesh, output):
-    for v in mesh.vertices:
-        output.extend(struct.pack("<fff", v.co[0], v.co[1], v.co[2]))
+    unique_verts = {}
+    vertex_list = []
+    vertex_indices = []
+    uvs = {}
+    normals = {}
     for t in mesh.loop_triangles:
-        output.extend(struct.pack("<III", t.vertices[0], t.vertices[1], t.vertices[2]))
-    if mesh.uv_layers.active:
-        n_uvs = 0
-        # TODO Maybe have support for different UVs on the same vertex position,
-        # Blender supports this but in the exporter we'd have to duplicate
-        # the vertex.
-        uv_data = array.array("f", range(0, len(mesh.vertices) * 2))
-        layer = mesh.uv_layers.active.data
-        for p in mesh.polygons:
-            for l in p.loop_indices:
-                v = mesh.loops[l].vertex_index
-                uv_data[v * 2] = layer[l].uv[0]
-                uv_data[v * 2 + 1] = layer[l].uv[1]
-        output.extend(uv_data)
+        for l in t.loops:
+            vert_idx = mesh.loops[l].vertex_index
+            uv = (mesh.uv_layers.active.data[l].uv[0], mesh.uv_layers.active.data[l].uv[1])
+            uv_idx = -1
+            if uv not in uvs:
+                uvs[uv] = l
+                uv_idx = l
+            else:
+                uv_idx = uvs[uv]
+                
+            normal = (mesh.loops[l].normal[0], mesh.loops[l].normal[1], mesh.loops[l].normal[2])
+            n_idx = -1
+            if normal not in normals:
+                normals[normal] = l
+                n_idx = l
+            else:
+                n_idx = normals[normal]
+
+            idx = (vert_idx, uv_idx, n_idx)
+            vid = -1
+            if not idx in unique_verts:
+                vid = len(vertex_list)
+                unique_verts[idx] = vid
+                vertex_list.append(idx)
+            else:
+                vid = unique_verts[idx]
+            vertex_indices.append(vid)
+            
+    for v in vertex_list:
+        vert = mesh.vertices[v[0]]
+        output.extend(struct.pack("<fff", vert.co[0], vert.co[1], vert.co[2]))
+    
+    for id in vertex_indices:
+        output.extend(struct.pack("<I", id))
+
+    for v in vertex_list:
+        uv = [mesh.uv_layers.active.data[v[1]].uv[0], mesh.uv_layers.active.data[v[1]].uv[1]]
+        output.extend(struct.pack("<ff", uv[0], uv[1]))
+
+    for v in vertex_list:
+        normal = [mesh.loops[v[2]].normal[0], mesh.loops[v[2]].normal[1], mesh.loops[v[2]].normal[2]]
+        output.extend(struct.pack("<fff", normal[0], normal[1], normal[2]))
 
 def export_crts(operator, scene, filepath=""):
     header = {
@@ -236,6 +307,15 @@ def export_crts(operator, scene, filepath=""):
         "materials": [],
         "images": []
     }
+
+    # Check no meshes are using multiple materials
+    for mesh in bpy.data.meshes:
+        if mesh.users == 0:
+            continue
+        if len(mesh.materials) > 1:
+            operator.report({"ERROR"},
+                    "Please split mesh '{}' into per-material meshes to ensure proper export".format(mesh.name))
+            return {"CANCELLED"}
 
     byte_offset, mesh_indices = write_mesh_info(bpy.data.meshes, header, 0)
     byte_offset, image_indices = write_image_info(bpy.data.images, header, byte_offset)
@@ -249,11 +329,17 @@ def export_crts(operator, scene, filepath=""):
         f.write(header_bytes)
         # Write mesh buffers
         for mesh in bpy.data.meshes:
+            if mesh.users == 0:
+                continue
+            
             buf = bytearray()
             write_mesh_buffers(mesh, buf)
             f.write(buf)               
         # Write image buffers
         for img in bpy.data.images:
+            if img.users == 0:
+                continue
+            
             if img.packed_file:
                 f.write(img.packed_file.data)
             else:
